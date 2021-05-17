6b233b610bf5b63cb67103e134f64534432e0f9f
rockygado <37998837+rockygado@users.noreply.github.com>
2021-05-18 02:38:35 +0300

    Add Movie Controller

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoRent.Models;
+
+namespace VideoRent.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+            return View(movie);
+        }
+    }
+}

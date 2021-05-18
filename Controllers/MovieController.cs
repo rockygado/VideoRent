6e5fce62b25021617c0d4feffed77ac8aa584389
rockygado <37998837+rockygado@users.noreply.github.com>
2021-05-18 03:50:21 +0300

    Add Routing to controller

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -14,6 +14,32 @@ namespace VideoRent.Controllers
         {
             var movie = new Movie() { Name = "Shrek" };
             return View(movie);
+            //return Content(movie.Name);
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home");
+        }
+
+        // GET: Movie/Edit/id
+        public ActionResult Edit(int id)
+        {
+            return Content("Id = " + id);
+        }
+
+        public ActionResult Index(int? pageIndex , string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(string.Format("This is page number {0} And sorted by {1}", pageIndex, sortBy));
+;       }
+        
+        [Route("Movie/Released/{year:regex(\\d{4}):min(1995):max(2021)}/{month:range(1,12)}")]
+        public ActionResult ByReleaseDate(int year,int month)
+        {
+            return Content(year+"/"+month);
         }
     }
 }
